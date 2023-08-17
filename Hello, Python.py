9022e52484db56f3df4dca144725241ae6bcb599
Hashim4x <hashim.go@gmail.com>
2023-08-17 13:56:13 +0300

    Update Hello, Python.py

--- Hello, Python.py
+++ Hello, Python.py
@@ -53,3 +53,18 @@ print(int(3.33))
 print(int('807') + 1)
 
 print(-a(1))
+
+
+
+def least_difference(1, 10, 100):
+    diff1 = abs(a - b)
+    diff2 = abs(b - c)
+    diff3 = abs(a - c)
+    reture min(diff1, diff2, diff3)
+
+print(
+    least_difference(1, 10, 100),
+    least_difference(1, 10, 10),
+    least_difference(5, 6, 7), # Python allows trailing commas in argument lists. How nice is that?
+
+
