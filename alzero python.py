22957db983ee1071556c3ba3a64717b2906031c8
Hashim4x <hashim.go@gmail.com>
2023-08-24 02:26:52 +0300

    Update alzero python.py

--- alzero python.py
+++ alzero python.py
@@ -8,3 +8,38 @@ print(a.split())
 
 b = "I-Love-Python"
 print(b.split())
+
+c = "I-Love-Python"
+print(c.split("-"))
+
+d = "I-Love-Python-and-R"
+print(d.split("-", 2))
+
+
+# center()
+
+e = "Hashim"
+print(e.center(10))       #spaces
+print(e.center(10, "$"))     #Hashes
+
+
+# count()
+
+f = "I Love Python and R Because R is Easy"
+print(f.count("R")) # 2 R 
+print(f.count("R", 0, 20))  # Only 1 R
+
+# sawpcase()
+
+g = "I Love Python"
+h = "i lOVE pYThon"
+
+print(g.swapcase())
+print(h.swapcase())
+
+
+# startswith()
+
+i = "I Love Python"
+print(i.startswith("I"))
+
