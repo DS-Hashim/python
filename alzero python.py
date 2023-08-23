f20f7b13abeaaa71c4c573ba8a74b4775e4eaf53
Hashim4x <hashim.go@gmail.com>
2023-08-24 01:53:53 +0300

    Update alzero python.py

--- alzero python.py
+++ alzero python.py
@@ -1,4 +1,10 @@
-a = "I Love Python"
-print(len(a))
+# a = "I Love Python"
+# print(len(a))
 
-print(" i dont what the fuck im doing ")
+# print(" i dont what the fuck im doing ")
+
+a = "I Love Python"
+print(a.split())
+
+b = "I-Love-Python"
+print(b.split())
