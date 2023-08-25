27da808d54650c671c09fe5a1629665e266767a9
Hashim4x <hashim.go@gmail.com>
2023-08-26 01:40:02 +0300

    Update alzero python.py

--- alzero python.py
+++ alzero python.py
@@ -1,45 +1,51 @@
+# # a = "I Love Python"
+# # print(len(a))
+
+# # print(" i dont what the fuck im doing ")
+
 # a = "I Love Python"
-# print(len(a))
+# print(a.split())
 
-# print(" i dont what the fuck im doing ")
+# b = "I-Love-Python"
+# print(b.split())
 
-a = "I Love Python"
-print(a.split())
+# c = "I-Love-Python"
+# print(c.split("-"))
 
-b = "I-Love-Python"
-print(b.split())
-
-c = "I-Love-Python"
-print(c.split("-"))
-
-d = "I-Love-Python-and-R"
-print(d.split("-", 2))
+# d = "I-Love-Python-and-R"
+# print(d.split("-", 2))
 
 
-# center()
+# # center()
 
-e = "Hashim"
-print(e.center(10))       #spaces
-print(e.center(10, "$"))     #Hashes
-
-
-# count()
-
-f = "I Love Python and R Because R is Easy"
-print(f.count("R")) # 2 R 
-print(f.count("R", 0, 20))  # Only 1 R
-
-# sawpcase()
-
-g = "I Love Python"
-h = "i lOVE pYThon"
-
-print(g.swapcase())
-print(h.swapcase())
+# e = "Hashim"
+# print(e.center(10))       #spaces
+# print(e.center(10, "$"))     #Hashes
 
 
-# startswith()
+# # count()
 
-i = "I Love Python"
-print(i.startswith("I"))
+# f = "I Love Python and R Because R is Easy"
+# print(f.count("R")) # 2 R 
+# print(f.count("R", 0, 20))  # Only 1 R
+
+# # sawpcase()
+
+# g = "I Love Python"
+# h = "i lOVE pYThon"
+
+# print(g.swapcase())
+# print(h.swapcase())
+
+
+# # startswith()
+
+# i = "I Love Python"
+# print(i.startswith("I"))
+
+
+# index (SubString, Start, End)
+
+a = "I Love Pyhton "
+print(a.index("P")) # index number 7
 
