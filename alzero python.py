5de194f59d4bcf9704113ac9218577185cef2011
Hashim4x <hashim.go@gmail.com>
2023-08-27 02:39:53 +0300

    Update alzero python.py

--- alzero python.py
+++ alzero python.py
@@ -46,6 +46,20 @@
 
 # index (SubString, Start, End)
 
-a = "I Love Pyhton "
-print(a.index("P")) # index number 7
+# a = "I Love Pyhton "
+# print(a.index("P")) # index number 7
 
+# replace (Old Value, New Value, Count)
+
+a = "Hello One Two Three One One"
+print(a.replace("One", "1"))
+
+print(a.replace("One", "1", 1))
+
+
+print(a.replace("One", "1", 2))
+
+# join(iterable)
+
+myList = ["Hashim", "Mohammed", "Nasser"]
+print(" ".join(myList))
